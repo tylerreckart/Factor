3fdf637903acec19c14113aedc1856b954df7b7d
Tyler Reckart <tyler.reckart@gmail.com>
2022-07-10 14:15:58 -0400

    update calculation method

--- Lumen/ContentView.swift
+++ Lumen/ContentView.swift
@@ -17,118 +17,83 @@ struct ContentView: View {
     @State private var extension_factor: String = "No compensation necessary"
     @State private var calculated_factor: Bool = false
     @State private var compensated_aperture: String = ""
+    @State private var priority_mode: String = "aperture"
 
     var body: some View {
-        let focal_length_binding = Binding<String>(get: {
-            self.focal_length
-        }, set: {
-            self.focal_length = $0
-            
-            let current_extension = Int(self.bellows_draw) ?? 0;
-            let focal_length = Int($0) ?? 1;
-            
-            let factor = Float(pow(Float(current_extension/focal_length), 2))
-            
-            if Int(self.bellows_draw) ?? 0 > 0 {
-                // (Extension/FocalLength) **2
-                if factor <= 1.0 {
-                    self.extension_factor = "\(factor)"
-                }
-                self.calculated_factor = true
-                
-                let aperture_compensation = log(factor)/log(2)
-                self.compensated_aperture = "\(aperture_compensation)"
-            } else if self.calculated_factor == true {
-                self.calculated_factor = false
-            }
-        })
-        
-        let bellows_draw_binding = Binding<String>(get: {
-            self.bellows_draw
-        }, set: {
-            self.bellows_draw = $0
-            
-            let current_extension = Int($0) ?? 1;
-            let focal_length = Int(self.focal_length) ?? 1;
-            
-            let factor = Float(pow(Float(current_extension/focal_length), 2))
-            
-            if Int(self.focal_length) ?? 0 > 0 {
-                // (Extension/FocalLength) **2
-                if factor <= 1.0 {
-                    self.extension_factor = "\(factor)"
-                }
-                self.calculated_factor = true
-                
-                let aperture_compensation = log(factor)/log(2)
-                self.compensated_aperture = "\(aperture_compensation * -(Float(aperture) ?? 1)!)"
-            } else if self.calculated_factor == true {
-                self.calculated_factor = false
-            }
-        })
-
         return NavigationView {
             VStack(alignment: .leading) {
                 GroupBox {
-                    Text("Exposure Data")
-                        .font(.system(.caption))
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .foregroundColor(.gray)
                     TextField("Aperture", text: $aperture)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                     TextField("Shutter Speed", text: $shutter_speed)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .disabled(aperture.isEmpty)
-                    TextField("Focal Length (mm)", text: focal_length_binding)
+                    TextField("Focal Length (mm)", text: $focal_length)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .keyboardType(.decimalPad)
                         .disabled(shutter_speed.isEmpty)
-                    TextField("Bellows Draw (mm)", text: bellows_draw_binding)
+                    TextField("Bellows Draw (mm)", text: $bellows_draw)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .keyboardType(.decimalPad)
                         .disabled(focal_length.isEmpty)
-                }
-                .navigationBarTitleDisplayMode(.inline)
-                .toolbar {
-                    ToolbarItem(placement: .principal) {
+                    
+                    VStack {
                         HStack {
-                            Image(systemName: "sun.min.fill")
-                            Text("Bellows Buddy")
-                                .font(.headline)
-                                .foregroundColor(.black)
-                            
-                        }
-                    }
-                }
-                
-                if Int(bellows_draw) ?? 0 != 0 || Int(focal_length) ?? 0 != 0 {
-                    GroupBox {
-                        HStack {
-                            if Int(focal_length) ?? 0 != 0 {
-                                VStack {
-                                    Text("Focal Length (mm)")
-                                        .font(.system(.caption))
-                                        .frame(maxWidth: .infinity, alignment: .leading)
-                                        .foregroundColor(.gray)
-                                    Text(focal_length)
-                                        .frame(maxWidth: .infinity, alignment: .leading)
-                                }
+                            Button(action: {
+                                self.priority_mode = "aperture"
+                            }) {
+                                Image(systemName: "camera.aperture")
+                                    .imageScale(.small)
+                                Text("Aperture Priority")
+                                    .font(.system(.caption))
+                                    .fontWeight(.bold)
                             }
-                            
-                            if Int(bellows_draw) ?? 0 != 0 {
-                                VStack {
-                                    Text("Bellows Draw (mm)")
-                                        .font(.system(.caption))
-                                        .frame(maxWidth: .infinity, alignment: .leading)
-                                        .foregroundColor(.gray)
-                                    Text(bellows_draw)
-                                        .frame(maxWidth: .infinity, alignment: .leading)
-                                }
-                            }
-                        }
-                    }
-                }
+                            .foregroundColor(self.priority_mode == "aperture" ? .white : .blue)
+                            .padding(12)
+                            .frame(maxWidth: .infinity)
+                            .background(self.priority_mode == "aperture" ? .blue : .clear)
+                            .cornerRadius(6)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 6)
+                                    .stroke(.blue, lineWidth: 1)
+                            )
 
+
+                            Button(action: {
+                                self.priority_mode = "shutter"
+                            }) {
+                                Image(systemName: "camera.shutter.button")
+                                    .imageScale(.small)
+                                Text("Shutter Priority")
+                                    .font(.system(.caption))
+                                    .fontWeight(.bold)
+                            }
+                            .foregroundColor(self.priority_mode == "shutter" ? .white : .blue)
+                            .padding(12)
+                            .frame(maxWidth: .infinity)
+                            .background(self.priority_mode == "shutter" ? .blue : .clear)
+                            .cornerRadius(6)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 6)
+                                    .stroke(.blue, lineWidth: 1)
+                            )
+                        }
+
+                        Button(action: calculate) {
+                            Image(systemName: "equal.square")
+                                .imageScale(.small)
+                            Text("Calculate")
+                                .font(.system(.caption))
+                                .fontWeight(.bold)
+                        }
+                        .foregroundColor(.white)
+                        .padding(12)
+                        .frame(maxWidth: .infinity)
+                        .background(.black)
+                        .cornerRadius(6)
+                    }
+                }
+    
                 if self.calculated_factor == true {
                     GroupBox {
                         VStack {
@@ -141,23 +106,27 @@ struct ContentView: View {
                         }
                         .padding(.bottom)
                         
-                        VStack {
-                            Text("Aperture Compensation")
-                                .font(.system(.caption))
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .foregroundColor(.gray)
-                            Text(compensated_aperture)
-                                .frame(maxWidth: .infinity, alignment: .leading)
+                        if (self.priority_mode == "aperture") {
+                            VStack {
+                                Text("Aperture Compensation")
+                                    .font(.system(.caption))
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .foregroundColor(.gray)
+                                Text(compensated_aperture)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                            }
+                            .padding(.bottom)
                         }
-                        .padding(.bottom)
 
-                        VStack {
-                            Text("Shutter Compensation")
-                                .font(.system(.caption))
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .foregroundColor(.gray)
-                            Text("32 seconds")
-                                .frame(maxWidth: .infinity, alignment: .leading)
+                        if (self.priority_mode == "shutter") {
+                            VStack {
+                                Text("Shutter Compensation")
+                                    .font(.system(.caption))
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .foregroundColor(.gray)
+                                Text("32 seconds")
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                            }
                         }
                     }
                 }
@@ -165,20 +134,25 @@ struct ContentView: View {
                 Spacer()
             }
             .padding()
-        }
-        .navigationBarTitleDisplayMode(.inline)
-        .toolbar {
-            ToolbarItem(placement: .cancellationAction) {
-                HStack {
-                    Image(systemName: "sun.min.fill")
-                    Text("Title").font(.headline)
-                }
-            }
+            .navigationBarTitle("Bellows Buddy", displayMode: .large)
         }
     }
     
-    static private func open_log() {
-        // do something.
+    public func calculate() {
+        let bellows_draw = Int(self.bellows_draw) ?? 1;
+        let focal_length = Int(self.focal_length) ?? 1;
+        
+        // (Extension/FocalLength) **2
+        let factor = Float(pow(Float(bellows_draw/focal_length), 2))
+        
+        if focal_length > 0 && bellows_draw > 0 {
+            self.extension_factor = "\(factor)"
+            self.calculated_factor = true
+            
+            let aperture_compensation = log(factor)/log(2)
+
+            self.compensated_aperture = "\(aperture_compensation * (Float(aperture) ?? 1)!)"
+        }
     }
 }
 
