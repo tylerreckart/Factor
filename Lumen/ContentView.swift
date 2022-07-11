a8d7706992679b554cb37b44ace17d8a1962121e
Tyler Reckart <tyler.reckart@gmail.com>
2022-07-10 20:05:53 -0400

    work on interactivity

--- Lumen/ContentView.swift
+++ Lumen/ContentView.swift
@@ -8,6 +8,35 @@
 import SwiftUI
 import CoreData
 
+struct DataFieldStyle: TextFieldStyle {
+    @Binding var focused: Bool
+    func _body(configuration: TextField<Self._Label>) -> some View {
+        configuration
+        .padding(10)
+        .background(
+            RoundedRectangle(cornerRadius: 8, style: .continuous)
+                .fill(focused ? .white : Color(.systemGray6))
+        )
+    }
+}
+
+struct GroupBoxWithFill: GroupBoxStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        VStack {
+            HStack {
+                configuration.label
+                    .font(.headline)
+                Spacer()
+            }
+            
+            configuration.content
+        }
+        .padding()
+        .background(RoundedRectangle(cornerRadius: 8, style: .continuous)
+            .fill(.white))
+    }
+}
+
 struct ContentView: View {
     @State private var aperture: String = ""
     @State private var iso: String = ""
@@ -18,24 +47,37 @@ struct ContentView: View {
     @State private var calculated_factor: Bool = false
     @State private var compensated_aperture: String = ""
     @State private var priority_mode: String = "aperture"
+    
+    @State private var aperture_input_focused: Bool = false
+    @State private var shutter_input_focused: Bool = false
+    @State private var focal_input_focused: Bool = false
+    @State private var draw_input_focused: Bool = false
 
     var body: some View {
         return NavigationView {
             VStack(alignment: .leading) {
-                GroupBox {
-                    TextField("Aperture", text: $aperture)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                    TextField("Shutter Speed", text: $shutter_speed)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .disabled(aperture.isEmpty)
-                    TextField("Focal Length (mm)", text: $focal_length)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                VStack {
+                    TextField("Aperture", text: $aperture, onEditingChanged: { edit in
+                        self.aperture_input_focused = edit
+                    })
+                        .textFieldStyle(DataFieldStyle(focused: $aperture_input_focused))
+                    
+                    TextField("Shutter Speed", text: $shutter_speed, onEditingChanged: { edit in
+                        self.shutter_input_focused = edit
+                    })
+                        .textFieldStyle(DataFieldStyle(focused: $shutter_input_focused))
+                    
+                    TextField("Focal Length (mm)", text: $focal_length, onEditingChanged: { edit in
+                        self.focal_input_focused = edit
+                    })
+                        .textFieldStyle(DataFieldStyle(focused: $focal_input_focused))
                         .keyboardType(.decimalPad)
-                        .disabled(shutter_speed.isEmpty)
-                    TextField("Bellows Draw (mm)", text: $bellows_draw)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                    TextField("Bellows Draw (mm)", text: $bellows_draw, onEditingChanged: { edit in
+                        self.draw_input_focused = edit
+                    })
+                        .textFieldStyle(DataFieldStyle(focused: $draw_input_focused))
                         .keyboardType(.decimalPad)
-                        .disabled(focal_length.isEmpty)
                     
                     VStack {
                         HStack {
@@ -68,14 +110,14 @@ struct ContentView: View {
                                     .font(.system(.caption))
                                     .fontWeight(.bold)
                             }
-                            .foregroundColor(self.priority_mode == "shutter" ? .white : .blue)
+                            .foregroundColor(self.priority_mode == "shutter" ? .white : .green)
                             .padding(12)
                             .frame(maxWidth: .infinity)
-                            .background(self.priority_mode == "shutter" ? .blue : .clear)
+                            .background(self.priority_mode == "shutter" ? .green : .clear)
                             .cornerRadius(6)
                             .overlay(
                                 RoundedRectangle(cornerRadius: 6)
-                                    .stroke(.blue, lineWidth: 1)
+                                    .stroke(.green, lineWidth: 1)
                             )
                         }
 
@@ -93,9 +135,14 @@ struct ContentView: View {
                         .cornerRadius(6)
                     }
                 }
+                .padding()
+                .background(.white)
+                .cornerRadius(8)
+                .shadow(color: Color.black.opacity(0.05), radius: 12, x: 0, y: 10)
+                .padding(.bottom)
     
                 if self.calculated_factor == true {
-                    GroupBox {
+                    VStack {
                         VStack {
                             Text("Bellows Extension Factor")
                                 .font(.system(.caption))
@@ -112,10 +159,13 @@ struct ContentView: View {
                                     .font(.system(.caption))
                                     .frame(maxWidth: .infinity, alignment: .leading)
                                     .foregroundColor(.gray)
-                                Text(compensated_aperture)
-                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                HStack {
+                                    Image(systemName: "f.cursive")
+                                        .imageScale(.medium)
+                                    Text(compensated_aperture)
+                                        .frame(maxWidth: .infinity, alignment: .leading)
+                                }
                             }
-                            .padding(.bottom)
                         }
 
                         if (self.priority_mode == "shutter") {
@@ -129,12 +179,16 @@ struct ContentView: View {
                             }
                         }
                     }
+                    .padding()
+                    .background(.white)
+                    .cornerRadius(8)
+                    .shadow(color: Color.black.opacity(0.05), radius: 12, x: 0, y: 10)
                 }
                 
                 Spacer()
             }
             .padding()
-            .navigationBarTitle("Bellows Buddy", displayMode: .large)
+            .background(Color(.systemGray6))
         }
     }
     
@@ -146,12 +200,12 @@ struct ContentView: View {
         let factor = Float(pow(Float(bellows_draw/focal_length), 2))
         
         if focal_length > 0 && bellows_draw > 0 {
-            self.extension_factor = "\(factor)"
+            self.extension_factor = "\(Int(factor))"
             self.calculated_factor = true
             
             let aperture_compensation = log(factor)/log(2)
 
-            self.compensated_aperture = "\(aperture_compensation * (Float(aperture) ?? 1)!)"
+            self.compensated_aperture = "\(Int(aperture_compensation * (Float(aperture) ?? 1)!))"
         }
     }
 }
