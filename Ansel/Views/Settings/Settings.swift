186c1b2c882c29a61885d1bcd947ebaf34bc2cd6
Tyler Reckart <tyler.reckart@gmail.com>
2022-08-25 21:20:02 -0400

    build out settings view

--- Ansel/Views/Settings/Settings.swift
+++ Ansel/Views/Settings/Settings.swift
@@ -11,12 +11,50 @@ struct Settings: View {
     var body: some View {
         VStack {
             List {
+                Section {
+                    NavigationLink(destination: EmptyView()) {
+                        VStack(alignment: .leading) {
+                            Text("Ansel Premium")
+                                .padding(.bottom, 1)
+                            Text("Premium Through December 25, 2045")
+                                .font(.system(size: 12))
+                                .textCase(.uppercase)
+                                .foregroundColor(Color(.systemGray))
+                        }
+                    }
+                }
+
                 Section(header: Text("Customization")) {
                     NavigationLink(destination: ThemeSettings()) {
                         Text("Theme")
                     }
                     NavigationLink(destination: AppIconSettings()) {
                         Text("App Icon")
+                    }
+                }
+                
+                Section(header: Text("Feedback")) {
+                    NavigationLink(destination: EmptyView()) {
+                        Text("Send Feedback")
+                    }
+                    NavigationLink(destination: EmptyView()) {
+                        VStack(alignment: .leading) {
+                            Text("Please Rate Ansel")
+                                .padding(.bottom, 1)
+                            Text("Thank you for your support")
+                                .font(.system(size: 12))
+                                .textCase(.uppercase)
+                                .foregroundColor(Color(.systemGray))
+                        }
+                    }
+                }
+                
+                Section {
+                    NavigationLink(destination: EmptyView()) {
+                        Text("Privacy")
+                    }
+                    NavigationLink(destination: EmptyView()) {
+                        Text("About")
                     }
                 }
             }
