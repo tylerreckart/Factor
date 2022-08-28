b06eb7bcd53fce398c36fc9b6196a7ce59f78d2d
Tyler Reckart <tyler.reckart@gmail.com>
2022-08-28 15:28:43 -0400

    more iap ui

--- Ansel/Views/Settings/Premium.swift
+++ Ansel/Views/Settings/Premium.swift
@@ -11,29 +11,47 @@ struct Premium: View {
     @State private var isPremium: Bool = false
 
     var body: some View {
-        NavigationView {
-            List {
-                Section(header:
-                    VStack(alignment: .leading) {
-                        if isPremium {
-                            Text("Thank you for supporting Ansel as a premium customer.")
-                                .padding(.bottom)
-                        }
-                        
-                        Text("Ansel Premium is an optional subscription that shows your support and funds future development. There are no additional features available to premium subscribers. Ansel's core feature set is and will always remain free to use.")
-                    }
-                    .padding(.bottom)
-                    .foregroundColor(Color(.systemGray))
-                    .font(.system(size: 14))
-                    .textCase(.none)
-                ) {
+        List {
+            Section(header:
+                VStack(alignment: .leading) {
                     if isPremium {
-                        Text("Change or Cancel Subscription")
-                    } else {
-                        Text("Subscribe for $9.99 per year")
+                        Text("Thank you for supporting Ansel as a premium customer.")
+                            .padding(.bottom)
                     }
                     
-                    Text("Restore Previous Purchases")
+                    Text("Ansel Premium is an optional subscription that shows your support and funds future development. There are no additional features available to premium subscribers. Ansel's core feature set is and will always remain free to use.")
+                }
+                .padding(.bottom)
+                .foregroundColor(Color(.systemGray))
+                .font(.system(size: 14))
+                .textCase(.none)
+            ) {
+                if isPremium {
+                    Text("Change or Cancel Subscription")
+                } else {
+                    Text("Subscribe for $9.99 per year")
+                }
+                
+                Text("Restore Previous Purchases")
+            }
+            
+            Section(header:
+                VStack(alignment: .leading) {
+                    Text("Payment will be charged to your Apple ID account at the confirmation of purchase. Subscription automatically renews unless it is canceled at least 24 hours before the end of the current period. Your account will be charged for renewal within 24 hours prior to the end of the current period.")
+                        .padding(.bottom)
+                    Text("You can manage and cancel your subscription by going to your account settigns in the App Store after purchase.")
+                }
+                .padding(.bottom)
+                .foregroundColor(Color(.systemGray))
+                .font(.system(size: 14))
+                .textCase(.none)
+            ) {
+                NavigationLink(destination: Privacy()) {
+                    Text("Privacy Policy")
+                }
+                
+                NavigationLink(destination: EmptyView()) {
+                    Text("Terms of Use")
                 }
             }
         }
